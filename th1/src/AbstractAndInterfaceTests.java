cdf33f10c979f44a39a79ac406efdcc615dc6785
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2023-01-15 02:16:16 +0700

    [Thực hành] Lớp Animal và interface Edible

--- th1/src/AbstractAndInterfaceTests.java
+++ th1/src/AbstractAndInterfaceTests.java
@@ -9,8 +9,8 @@ import fruit.Apple;
 public class AbstractAndInterfaceTests {
     public static void main(String[] args) {
         Animal[] animals = new Animal[2];
-        animals[0] = new Tiger();
-        animals[1] = new Chicken();
+        animals[1] = new Tiger();
+        animals[0] = new Chicken();
         for (Animal animal : animals) {
             System.out.println(animal.makeSound());
 
